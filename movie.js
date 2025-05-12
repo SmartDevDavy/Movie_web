0801ef3f8746510369a6deee2bb1d784cc05ebb8
SmartDevDavy <davykamau8@gmail.com>
2025-05-12 13:33:54 +0300

    All movies should be added

---
+++ movie.js
@@ -0,0 +1,229 @@
+const movie = document.querySelectorAll(".image");
+const vid = document.querySelector("#vid");
+const vidVid = document.querySelector("#vid video");
+const close = document.querySelector("#close");
+/*
+const 1 = document.querySelector(".image1");
+const 2 = document.querySelector(".image2");
+const 3 = document.querySelector(".image3");
+const 4 = document.querySelector(".image4");
+const 5 = document.querySelector(".image5");
+const 6 = document.querySelector(".image6");
+const 7 = document.querySelector(".image7");
+const 8 = document.querySelector(".image8");
+const 9 = document.querySelector(".image9");
+const 10 = document.querySelector(".image10");
+const 11 = document.querySelector(".image11");
+const 12 = document.querySelector(".image12");
+const 13 = document.querySelector(".image13");
+const 14 = document.querySelector(".image14");
+const 15 = document.querySelector(".image15");
+const 16 = document.querySelector(".image16");
+const 17 = document.querySelector(".image17");
+const 18 = document.querySelector(".image18");
+const 19 = document.querySelector(".image19");
+const 20 = document.querySelector(".image20");
+const 21 = document.querySelector(".image21");
+const 22 = document.querySelector(".image22");
+const 23 = document.querySelector(".image23");
+const 24 = document.querySelector(".image24");
+const 25 = document.querySelector(".image25");
+const 26 = document.querySelector(".image26");
+const 27 = document.querySelector(".image27");
+const 28 = document.querySelector(".image28");
+const 29 = document.querySelector(".image29");
+const 30 = document.querySelector(".image30");
+const 31 = document.querySelector(".image31");
+const 32 = document.querySelector(".image32");
+const 33 = document.querySelector(".image33");
+const 34 = document.querySelector(".image34");
+const 35 = document.querySelector(".image35");
+const 36 = document.querySelector(".image36");
+const 37 = document.querySelector(".image37");
+const 38 = document.querySelector(".image38");
+const 39 = document.querySelector(".image39");
+const 40 = document.querySelector(".image40");
+const 41 = document.querySelector(".image41");
+const 42 = document.querySelector(".image42");
+
+*/
+movie.forEach(image => {
+    image.addEventListener("click", () => {
+            vid.showModal();
+    }); 
+});
+
+
+
+close.addEventListener("click", () => {
+   
+    vid.close();
+});
+
+
+/*
+
+
+
+
+
+switch (movie) {
+    case 1:
+        vidVid.source.src = "miviesvids\1.mp4";
+        break;
+
+        case 2:
+        
+        break;
+
+        case 3:
+        
+        break;
+
+        case 4:
+        
+        break;
+
+        case 5:
+        
+        break;
+
+        case 6:
+        
+        break;
+
+        case 7:
+        
+        break;
+
+        case 8:
+        
+        break;
+
+        case 9:
+        
+        break;
+
+        case 10:
+        
+        break;
+
+        case 11:
+        
+        break;
+
+        case 12:
+        
+        break;
+
+        case 13:
+        
+        break;
+
+        case 14:
+        
+        break;
+
+        case 15:
+        
+        break;
+
+        case 16:
+        
+        break;
+
+        case 17:
+        
+        break;
+
+        case 18:
+        
+        break;
+
+        case 19:
+        
+        break;
+
+        case 20:
+        
+        break;
+
+        case 21:
+        
+        break;
+
+        case 22:
+        
+        break;
+
+        case 23:
+        
+        break;
+
+        case 24:
+        
+        break;
+
+        case 25:
+        
+        break;
+
+        case 26:
+        
+        break;
+
+        case 27:
+        
+        break;
+
+        case 28:
+        
+        break;
+
+        case 29:
+        
+        break;
+
+        case 30:
+        
+        break;
+
+        case 24:
+        
+        break;
+
+        case 24:
+        
+        break;
+
+        case 24:
+        
+        break;
+
+
+        case 24:
+        
+        break;
+
+        case 24:
+        
+        break;
+
+        case 24:
+        
+        break;
+
+        case 24:
+        
+        break;
+
+        case 24:
+        
+        break;
+
+    default:
+        break;
+}
+
+
+*/
